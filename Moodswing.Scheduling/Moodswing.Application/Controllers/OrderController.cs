9894b54047e93bd9adf6b70c366c0166797473c2
andreymmonteiro <andreymuti@hotmail.com>
2022-08-12 09:53:38 -0300

    Template

---
+++ Moodswing.Scheduling/Moodswing.Application/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Moodswing.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetStatusOkAsync()
+        {
+            return Ok();
+        }
+    }
+}
