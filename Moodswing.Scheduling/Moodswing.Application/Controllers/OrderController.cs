1c1e758d91b1f9c882af410c1f8e329a07555346
andreymmonteiro <andreymuti@hotmail.com>
2022-08-12 12:02:03 -0300

    Included fake response

--- Moodswing.Scheduling/Moodswing.Application/Controllers/OrderController.cs
+++ Moodswing.Scheduling/Moodswing.Application/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Moodswing.Application.Controllers
 {
@@ -10,7 +11,24 @@ namespace Moodswing.Application.Controllers
         [HttpGet]
         public IActionResult GetStatusOkAsync()
         {
-            return Ok();
+            return Ok(new List<Dictionary<string, string>>()
+            {
+              new Dictionary<string, string>()
+                {
+                    { "name", "Andrey Monteiro" },
+                    { "cel", "Boston" }
+                },
+              new Dictionary<string, string>()
+                {
+                    { "name", "Gideval Santos" },
+                    { "cel", "Boston" }
+                },
+              new Dictionary<string, string>()
+                {
+                    { "name", "Matheus Hoffman" },
+                    { "cel", "Boston" }
+                }
+            });
         }
     }
 }
