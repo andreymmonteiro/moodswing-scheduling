aa102d415de44d341731282cde56d4529cf41dcf
andreymmonteiro <andreymuti@hotmail.com>
2022-08-16 11:10:05 -0300

    Included auth

---
+++ Moodswing.Scheduling/Moodswing.Domain/Models/Authentication/Token.cs
@@ -0,0 +1,16 @@
+﻿namespace Moodswing.Domain.Models.Authentication
+{
+    public struct Token
+    {
+        private readonly string _token;
+
+        private Token(string token)
+            => _token = token;
+
+        public static implicit operator Token(string token)
+            => new(token);
+        
+        public string GetToken
+            => _token;
+    }
+}
---
+++ Moodswing.Scheduling/Moodswing.Service/Authentication/SigningConfigurations.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Moodswing.Domain.Models.Authentication;
+using System.Text;
+
+namespace Moodswing.Service.Authentication
+{
+    public class SigningConfigurations : ISigningConfigurations
+    {
+        private const string SECRET = "privateKeyLittleTalks";
+
+        public SecurityKey Key { get; set; }
+
+        public SigningCredentials SigningCredentials { get; set; }
+
+        public SigningConfigurations()
+        {
+            var provider = Encoding.ASCII.GetBytes(SECRET);
+            Key = new SymmetricSecurityKey(provider);
+
+            SigningCredentials = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256Signature);
+        }
+    }
+}
